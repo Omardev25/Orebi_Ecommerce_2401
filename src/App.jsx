d00d4727b3deac6b1940359305d51e561bfa0362
Omardev25 <omardev26@gmail.com>
2026-01-02 14:46:35 +0600

    React-router-dome set up is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider
+} from "react-router-dom";
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Product from './Pages/Product';
+import Error from './Pages/Error';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/product" element={<Product />}></Route>
+      <Route path="/*" element={<Error />}></Route>
+    </Route>
+
+  )
+);
+
 
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
+    <RouterProvider router={router} />
+  
   )
 }
 
